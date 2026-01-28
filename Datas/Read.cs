b983315375f52ba3c734366bd7cad3c95fcd6bce
FZoltan40 <farkasz@kkszki.hu>
2026-01-28 09:40:38 +0100

    Read is created.

---
+++ Datas/Read.cs
@@ -0,0 +1,23 @@
+﻿using LibraryGUI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryGUI.Datas
+{
+    public class Read
+    {
+        public List<Authors> ReadAuthors() 
+        {
+            using (var context = new librarydbContext())
+            {
+                var users = context.Authors.ToList();
+                return users;
+            }
+        }
+    }
+}
---
+++ Views/ShowDatas.xaml.cs
@@ -0,0 +1,29 @@
+﻿using LibraryGUI.Datas;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace LibraryGUI.Views
+{
+    /// <summary>
+    /// Interaction logic for ShowDatas.xaml
+    /// </summary>
+    public partial class ShowDatas : Page
+    {
+        Read read = new Read();
+        private readonly MainWindow _mainWindow;
+        public ShowDatas(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+            
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var list = read.ReadAuthors();
+            MessageBox.Show(list?.Count.ToString() ?? "null");
+            dataGrid1.ItemsSource = list;
+        }
+    }
+}
